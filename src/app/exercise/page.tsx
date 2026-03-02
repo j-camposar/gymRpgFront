c7a5a7c4f9f9ebf07a82ef6542b80885b460af23
j-camposar <31163773+j-camposar@users.noreply.github.com>
2026-03-02 02:57:53 -0300

    correccion de despiste

--- src/app/exercise/page.tsx
+++ src/app/exercise/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 import ListaExercise from '@/component/exercise/ListaExercise';
 import { Exercise } from '@/types/exercise';
+import { HistorySeries } from '@/types/training';
 import { useState } from 'react';
 
 export default function ExercisesPage() {
     const [selectedExercise, setSelectedExercise] = useState<Exercise | null>(null);
-    const [ultimoRegistro, setUltimoRegistro] = useState<Exercise | null>(null);
+    const [ultimoRegistro, setUltimoRegistro] = useState<HistorySeries | null>(null);
     return (<ListaExercise setSelectedExercise={setSelectedExercise} setUltimoRegistro={setUltimoRegistro}  />);
 }
